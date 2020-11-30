921738631e9e0b7f2d8b66fda979976b0b32ef26
2Guib <maxime.deguibert@orange.fr>
2020-11-30 20:53:36 +0100

    covertion nombre vers romain et calculatrice romain vers romain + test

--- romain/romain.py
+++ romain/romain.py
@@ -38,3 +38,58 @@ def calculatrice(operateur, romain1, romain2):
     elif operateur == '/':
         return romain_to_num(romain1) / romain_to_num(romain2)
     return 0
+
+
+def num_to_romain(num):
+    res = ""
+    if num<0:
+        num*=-1
+        res+="-"
+    while num >= valeur_de('M'):
+        res += "M"
+        num -= valeur_de('M')
+    if num >= valeur_de('C') * 9:
+        res += "CM"
+        num -= romain_to_num("CM")
+    while num >= valeur_de('D'):
+        res += "D"
+        num -= valeur_de('D')
+    if num >= valeur_de('C') * 4:
+        res += "CD"
+        num -= romain_to_num("CD")
+    while num >= valeur_de('C'):
+        res += "C"
+        num -= valeur_de('C')
+    if num >= valeur_de('X') * 9:
+        res += "XC"
+        num -= romain_to_num("XC")
+    while num >= valeur_de('L'):
+        res += "L"
+        num -= valeur_de('L')
+    if num >= valeur_de('X') * 4:
+        res += "XL"
+        num -= romain_to_num("XL")
+    while num >= valeur_de('X'):
+        res += "X"
+        num -= valeur_de('X')
+    if num >= valeur_de('I') * 9:
+        res += "IX"
+        num -= romain_to_num("IX")
+    while num >= valeur_de('V'):
+        res += "V"
+        num -= valeur_de('V')
+    if num >= valeur_de('I') * 4:
+        res += "IV"
+        num -= romain_to_num("IV")
+    while num >= valeur_de('I'):
+        res += "I"
+        num -= valeur_de('I')
+
+    if num == 0:
+        return res
+    else:
+        return -1
+
+
+def calculatrice_romain(operateur, romain1, romain2):
+    return num_to_romain(calculatrice(operateur, romain1, romain2))
--- test/test_romain.py
+++ test/test_romain.py
@@ -69,11 +69,13 @@ def test_calculatrice_addition_chiffres():
     assert r.calculatrice('+', "D", "C") == 600
     assert r.calculatrice('+', "D", "M") == 1500
 
+
 def test_calculatrice_addition_nombres():
     assert r.calculatrice('+', "IV", "") == 4
     assert r.calculatrice('+', "IV", "VI") == 10
     assert r.calculatrice('+', "XIV", "LVI") == 70
     assert r.calculatrice('+', "III", "MCMXLIV") == 1947
+
 
 def test_calculatrice_soustraction_chiffres():
     assert r.calculatrice('-', "", "") == 0
@@ -83,6 +85,7 @@ def test_calculatrice_soustraction_chiffres():
     assert r.calculatrice('-', "V", "I") == 4
     assert r.calculatrice('-', "I", "V") == -4
 
+
 def test_calculatrice_soustraction_nombres():
     assert r.calculatrice('-', "IV", "VI") == -2
     assert r.calculatrice('-', "VI", "IV") == 2
@@ -90,6 +93,7 @@ def test_calculatrice_soustraction_nombres():
     assert r.calculatrice('-', "XI", "VI") == 5
     assert r.calculatrice('-', "MCMXLIV", "XLIV") == 1900
     assert r.calculatrice('-', "XL", "MCMXLIV") == -1904
+
 
 def test_calculatrice_multiplication_chiffres():
     assert r.calculatrice('*', "I", "I") == 1
@@ -100,12 +104,14 @@ def test_calculatrice_multiplication_chiffres():
     assert r.calculatrice('*', "V", "X") == 50
     assert r.calculatrice('*', "X", "V") == 50
 
+
 def test_calculatrice_multiplication_nombres():
     assert r.calculatrice('*', "IV", "VI") == 24
     assert r.calculatrice('*', "VI", "IV") == 24
     assert r.calculatrice('*', "VI", "") == 0
     assert r.calculatrice('*', "VI", "I") == 6
     assert r.calculatrice('*', "MCMXLIV", "X") == 19440
+
 
 def test_calculatrice_division_chiffres():
     assert r.calculatrice('/', "I", "I") == 1
@@ -115,17 +121,83 @@ def test_calculatrice_division_chiffres():
     assert r.calculatrice('/', "V", "X") == 0.5
     assert r.calculatrice('/', "X", "V") == 2
 
+
 def test_calculatrice_division_nombres():
     assert r.calculatrice('/', "VI", "II") == 3
     assert r.calculatrice('/', "IV", "II") == 2
     assert r.calculatrice('/', "MCMXLIV", "IV") == 486
-    assert r.calculatrice('/', "I", "III") == 1/3
+    assert r.calculatrice('/', "I", "III") == 1 / 3
     assert r.calculatrice('/', "XL", "V") == 8
+
 
 def test_calculatrice_division_par_0():
     with pytest.raises(ZeroDivisionError):
         assert r.calculatrice('/', "X", "")
 
+
 def test_calculatrice_operateur_invalide():
     assert r.calculatrice('E', "XL", "V") == 0
     assert r.calculatrice('', "XL", "V") == 0
+
+
+def test_num_vers_romain_chiffre():
+    assert r.num_to_romain(1000) == "M"
+    assert r.num_to_romain(500) == "D"
+    assert r.num_to_romain(100) == "C"
+    assert r.num_to_romain(50) == "L"
+    assert r.num_to_romain(10) == "X"
+    assert r.num_to_romain(5) == "V"
+    assert r.num_to_romain(1) == "I"
+
+
+def test_num_vers_romain_double():
+    assert r.num_to_romain(3000) == "MMM"
+    assert r.num_to_romain(300) == "CCC"
+    assert r.num_to_romain(30) == "XXX"
+    assert r.num_to_romain(3) == "III"
+
+
+def test_num_vers_romain_sous_chiffres():
+    assert r.num_to_romain(4) == "IV"
+    assert r.num_to_romain(40) == "XL"
+    assert r.num_to_romain(400) == "CD"
+    assert r.num_to_romain(900) == "CM"
+    assert r.num_to_romain(90) == "XC"
+    assert r.num_to_romain(9) == "IX"
+
+
+def test_num_vers_romain_complexe():
+    assert r.num_to_romain(1944) == "MCMXLIV"
+    assert r.num_to_romain(2999) == "MMCMXCIX"
+    assert r.num_to_romain(4732) == "MMMMDCCXXXII"
+
+
+def test_calculatrice_romain_addition():
+    assert r.calculatrice_romain('+', "IV", "") == "IV"
+    assert r.calculatrice_romain('+', "IV", "VI") == "X"
+    assert r.calculatrice_romain('+', "XIV", "LVI") == "LXX"
+    assert r.calculatrice_romain('+', "III", "MCMXLIV") == "MCMXLVII"
+
+def test_calculatrice_romain_soustraction_nombres():
+    assert r.calculatrice_romain('-', "IV", "VI") == "-II"
+    assert r.calculatrice_romain('-', "VI", "IV") == "II"
+    assert r.calculatrice_romain('-', "IV", "IV") == ""
+    assert r.calculatrice_romain('-', "XI", "VI") == "V"
+    assert r.calculatrice_romain('-', "MCMXLIV", "XLIV") == "MCM"
+    assert r.calculatrice_romain('-', "XL", "MCMXLIV") == "-MCMIV"
+
+def test_calculatrice_romain_multiplication_nombres():
+    assert r.calculatrice_romain('*', "IV", "VI") == "XXIV"
+    assert r.calculatrice_romain('*', "VI", "IV") == "XXIV"
+    assert r.calculatrice_romain('*', "VI", "") == ""
+    assert r.calculatrice_romain('*', "VI", "I") == "VI"
+
+def test_calculatrice_romain_division_nombres():
+    assert r.calculatrice_romain('/', "VI", "II") == "III"
+    assert r.calculatrice_romain('/', "IV", "II") == "II"
+    assert r.calculatrice_romain('/', "MCMXLIV", "IV") == "CDLXXXVI"
+    assert r.calculatrice_romain('/', "XL", "V") == "VIII"
+
+def test_calculatrice_division_par_0():
+    with pytest.raises(ZeroDivisionError):
+        assert r.calculatrice_romain('/', "X", "")
